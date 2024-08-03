f51be1cb16093d45b5be655d0d5437507b6335c8
Euller Henrique Bandeira Oliveira <euller.oliveira@arphoenix.com.br>
2024-08-03 01:27:45 -0300

    feat: Adiciona constantes

--- src/main/java/com/api/pagamento/domain/constant/utils/txt/TxtConstants.java
+++ src/main/java/com/api/pagamento/domain/constant/utils/txt/TxtConstants.java
@@ -1,12 +1,12 @@
 package com.api.pagamento.domain.constant.utils.txt;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class TxtConstants {
 
 	public static final String ESPACO = " ";
 	public static final String PONTO = ".";
 	public static final String DOIS_PONTOS = ":";
 
-	// Construtor privado para impedir a criação de instâncias
-	private TxtConstants() {
-	}
 }

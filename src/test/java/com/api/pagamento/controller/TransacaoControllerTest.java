f050ba7c7ea2ff3774ac4cbaee405ff5ad946299
Euller Henrique Bandeira Oliveira <euller.oliveira@arphoenix.com.br>
2024-08-04 01:32:03 -0300

    feat: Quinto teste

--- src/test/java/com/api/pagamento/controller/TransacaoControllerTest.java
+++ src/test/java/com/api/pagamento/controller/TransacaoControllerTest.java
@@ -6,6 +6,7 @@ import com.api.pagamento.domain.builder.request.transacao.TransacaoRequestDtoBui
 import com.api.pagamento.domain.builder.response.transacao.TransacaoResponseDtoBuilder;
 import com.api.pagamento.domain.dto.request.transacao.TransacaoRequestDto;
 import com.api.pagamento.domain.dto.response.transacao.TransacaoResponseDto;
+import com.api.pagamento.domain.enumeration.transacao.descricao.StatusTransacaoEnum;
 import com.api.pagamento.domain.exception.handler.http.HttpExceptionHandler;
 import com.api.pagamento.domain.exception.http.NotFoundException;
 import com.api.pagamento.service.dto.transacao.TransacaoDtoService;
@@ -20,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -54,7 +56,6 @@ class TransacaoControllerTest {
 
 	@Test
 	void QuandoUmaTransacaoEhSolicitadaElaDeveSerRealizada() throws Exception {
-
 		//Dado
 		TransacaoRequestDto transacaoRequestDto = TransacaoRequestDtoBuilder.toTransacaoRequestDto();
 		TransacaoResponseDto transacaoResponseDto = TransacaoResponseDtoBuilder.toTransacaoResponseDto();
@@ -65,11 +66,9 @@ class TransacaoControllerTest {
 		// Então
 		String jsonResponse = gson.toJson(transacaoResponseDto);
 		mockMvc.perform(post("/transacao/v1/pagar").contentType(MediaType.APPLICATION_JSON).content(jsonResponse))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.id", is(transacaoResponseDto.getId())))
+				.andExpect(status().isOk()).andExpect(jsonPath("$.id", is(transacaoResponseDto.getId())))
 				.andExpect(jsonPath("$.cartao", is(transacaoResponseDto.getCartao())))
-				.andExpect(jsonPath("$.descricao.valor", is(transacaoResponseDto.getDescricao().getValor())))
-				.andExpect(jsonPath("$.descricao.dataHora", is(transacaoResponseDto.getDescricao().getDataHora().format(FORMATTER_DATA_HORA_PT_BR))))
+				.andExpect(jsonPath("$.descricao.valor", is(transacaoResponseDto.getDescricao().getValor()))).andExpect(jsonPath("$.descricao.dataHora", is(transacaoResponseDto.getDescricao().getDataHora().format(FORMATTER_DATA_HORA_PT_BR))))
 				.andExpect(jsonPath("$.descricao.estabelecimento", is(transacaoResponseDto.getDescricao().getEstabelecimento())))
 				.andExpect(jsonPath("$.descricao.nsu", is(transacaoResponseDto.getDescricao().getNsu())))
 				.andExpect(jsonPath("$.descricao.codigoAutorizacao", is(transacaoResponseDto.getDescricao().getCodigoAutorizacao())))
@@ -80,22 +79,19 @@ class TransacaoControllerTest {
 
 	@Test
 	void QuandoUmaTransacaoEhBuscadaPeloIdENaoEhEncontradaUmaExcecaoDeveSerRetornada() throws Exception {
-
-		// Dado
+		//Dado
 		Long id = 1L;
 
-		//When
+		//Quando
 		when(transacaoService.buscarTransacao(id)).thenThrow(NotFoundException.class);
 
 		// Então
 		mockMvc.perform(get("/transacao/v1/buscar/{id}", 1).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound())
 				.andExpect(result -> assertTrue(result.getResolvedException() instanceof NotFoundException));
-
 	}
 
 	@Test
 	void QuandoUmaTransacaoEhBuscadaPeloIdElaDeveSerRetornada() throws Exception {
-
 		//Dado
 		Long id = 1L;
 		TransacaoResponseDto transacaoResponseDto = TransacaoResponseDtoBuilder.toTransacaoResponseDto();
@@ -106,11 +102,9 @@ class TransacaoControllerTest {
 		// Então
 		String jsonResponse = gson.toJson(transacaoResponseDto);
 		mockMvc.perform(get("/transacao/v1/buscar/{id}", id).contentType(MediaType.APPLICATION_JSON).content(jsonResponse))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.id", is(transacaoResponseDto.getId())))
+				.andExpect(status().isOk()).andExpect(jsonPath("$.id", is(transacaoResponseDto.getId())))
 				.andExpect(jsonPath("$.cartao", is(transacaoResponseDto.getCartao())))
-				.andExpect(jsonPath("$.descricao.valor", is(transacaoResponseDto.getDescricao().getValor())))
-				.andExpect(jsonPath("$.descricao.dataHora", is(transacaoResponseDto.getDescricao().getDataHora().format(FORMATTER_DATA_HORA_PT_BR))))
+				.andExpect(jsonPath("$.descricao.valor", is(transacaoResponseDto.getDescricao().getValor()))).andExpect(jsonPath("$.descricao.dataHora", is(transacaoResponseDto.getDescricao().getDataHora().format(FORMATTER_DATA_HORA_PT_BR))))
 				.andExpect(jsonPath("$.descricao.estabelecimento", is(transacaoResponseDto.getDescricao().getEstabelecimento())))
 				.andExpect(jsonPath("$.descricao.nsu", is(transacaoResponseDto.getDescricao().getNsu())))
 				.andExpect(jsonPath("$.descricao.codigoAutorizacao", is(transacaoResponseDto.getDescricao().getCodigoAutorizacao())))
@@ -121,111 +115,76 @@ class TransacaoControllerTest {
 
 	@Test
 	void QuandoTransacoesSaoBuscadasElasDevemSerRetornadas() throws Exception {
-
 		//Dado
 		List<TransacaoResponseDto> transacoesResponseDto = new ArrayList<>();
 		for (int i = 0; i < 10; i++) {
 			transacoesResponseDto.add(TransacaoResponseDtoBuilder.toTransacaoResponseDto());
 		}
+
 		//Quando
 		when(transacaoService.listarTransacoes()).thenReturn(transacoesResponseDto);
 
 		// Então
 		String jsonResponse = gson.toJson(transacoesResponseDto);
-		mockMvc.perform(get("/transacao/v1/listar").contentType(MediaType.APPLICATION_JSON).content(jsonResponse))
-				.andExpect(jsonPath("$[*].id", containsInAnyOrder(transacoesResponseDto.stream().map(TransacaoResponseDto::getId).toArray())))
-				.andExpect(jsonPath("$[*].cartao", containsInAnyOrder(transacoesResponseDto.stream().map(TransacaoResponseDto::getCartao).toArray())))
-				.andExpect(jsonPath("$[*].descricao.valor", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getValor()).toArray())))
-				.andExpect(jsonPath("$[*].descricao.dataHora", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getDataHora().format(FORMATTER_DATA_HORA_PT_BR)).toArray())))
-				.andExpect(jsonPath("$[*].descricao.estabelecimento", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getEstabelecimento()).toArray())))
-				.andExpect(jsonPath("$[*].descricao.nsu", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getNsu()).toArray())))
-				.andExpect(jsonPath("$[*].descricao.codigoAutorizacao", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getCodigoAutorizacao()).toArray())))
-				.andExpect(jsonPath("$[*].descricao.status", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getStatus().toString()).toArray())))
-				.andExpect(jsonPath("$[*].formaPagamento.tipo", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getFormaPagamento().getTipo().toString()).toArray())))
-				.andExpect(jsonPath("$[*].formaPagamento.parcelas", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getFormaPagamento().getParcelas()).toArray())));
+		mockMvc.perform(get("/transacao/v1/listar").contentType(MediaType.APPLICATION_JSON).content(jsonResponse)).andExpect(jsonPath("$[*].id", containsInAnyOrder(transacoesResponseDto.stream().map(TransacaoResponseDto::getId).toArray())))
+				.andExpect(jsonPath("$[*].cartao", containsInAnyOrder(transacoesResponseDto.stream().map(TransacaoResponseDto::getCartao).toArray()))).andExpect(
+						jsonPath("$[*].descricao.valor", containsInAnyOrder(
+								transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getValor()).toArray()))).andExpect(jsonPath("$[*].descricao.dataHora", containsInAnyOrder(
+						transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getDataHora().format(FORMATTER_DATA_HORA_PT_BR))
+								.toArray()))).andExpect(jsonPath("$[*].descricao.estabelecimento", containsInAnyOrder(
+						transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getEstabelecimento()).toArray()))).andExpect(jsonPath("$[*].descricao.nsu", containsInAnyOrder(
+						transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getNsu()).toArray())))
+				.andExpect(jsonPath("$[*].descricao.codigoAutorizacao", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getCodigoAutorizacao()).toArray()))).andExpect(
+						jsonPath("$[*].descricao.status", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getDescricao().getStatus().toString()).toArray()))).andExpect(
+						jsonPath("$[*].formaPagamento.tipo", containsInAnyOrder(transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getFormaPagamento().getTipo().toString()).toArray()))).andExpect(
+						jsonPath("$[*].formaPagamento.parcelas", containsInAnyOrder(
+								transacoesResponseDto.stream().map(transacaoResponseDto -> transacaoResponseDto.getFormaPagamento().getParcelas()).toArray())));
+	}
+
+	@Test
+	void QuandoUmEstornoEhSolicitadoEleEhRealizado() throws Exception {
+
+		// Dado
+		Long id = 1L;
+		TransacaoResponseDto transacaoResponseDto = TransacaoResponseDtoBuilder.toTransacaoResponseDto();
+		transacaoResponseDto.getDescricao().setStatus(StatusTransacaoEnum.CANCELADO);
+
+		//When
+		when(transacaoService.estornar(id)).thenReturn(transacaoResponseDto);
+
+		// Então
+		String jsonResponse = gson.toJson(transacaoResponseDto);
+		mockMvc.perform(put("/transacao/v1/estornar/{id}", id).contentType(MediaType.APPLICATION_JSON).content(jsonResponse))
+				.andExpect(status().isOk()).andExpect(jsonPath("$.id", is(transacaoResponseDto.getId())))
+				.andExpect(jsonPath("$.cartao", is(transacaoResponseDto.getCartao())))
+				.andExpect(jsonPath("$.descricao.valor", is(transacaoResponseDto.getDescricao().getValor()))).andExpect(jsonPath("$.descricao.dataHora", is(transacaoResponseDto.getDescricao().getDataHora().format(FORMATTER_DATA_HORA_PT_BR))))
+				.andExpect(jsonPath("$.descricao.estabelecimento", is(transacaoResponseDto.getDescricao().getEstabelecimento())))
+				.andExpect(jsonPath("$.descricao.nsu", is(transacaoResponseDto.getDescricao().getNsu())))
+				.andExpect(jsonPath("$.descricao.codigoAutorizacao", is(transacaoResponseDto.getDescricao().getCodigoAutorizacao())))
+				.andExpect(jsonPath("$.descricao.status", is(StatusTransacaoEnum.CANCELADO.toString())))
+				.andExpect(jsonPath("$.formaPagamento.tipo", is(transacaoResponseDto.getFormaPagamento().getTipo().toString())))
+				.andExpect(jsonPath("$.formaPagamento.parcelas", is(transacaoResponseDto.getFormaPagamento().getParcelas())));
+
+	}
+
+	@Test
+	void QuandoUmPagamentoEhSolicitadoComCamposPreenchidosIncorretamenteUmaExcecaoDeveSerRetornada() throws Exception {
+
+		// Dado
+		TransacaoResponseDto transacaoResponseDto = TransacaoResponseDtoBuilder.toTransacaoResponseDto();
+		transacaoResponseDto.setCartao(null);
+		transacaoResponseDto.getDescricao().setEstabelecimento(null);
+
+		//When
+		//A anotação @Valid do Spring não permite que o objeto seja instanciado com campos nulos
+
+		// Então
+		String jsonResponse = gson.toJson(transacaoResponseDto);
+		mockMvc.perform(post("/transacao/v1/pagar").contentType(MediaType.APPLICATION_JSON).content(jsonResponse))
+				.andExpect(status().isBadRequest())
+				.andExpect(result -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException));;
+
 	}
 
 }
-
-	/*
-    // Quando um pagamento não é chamado com todos os campos obrigatórios, uma exceção deve ser retornada
-    @Test
-    void whenPaymentWithoutAllFieldsIsCalledThenAnExceptionIsReturned() throws Exception {
-
-        // Dado
-
-        //Gera um TransacaoDTO
-        TransacaoDTO transacaoDTO = TransacaoDTOBuilder.builder().build().toTransacaoDTO();
-
-        transacaoDTO.setCartao(null);
-        transacaoDTO.getDescricao().setEstabelecimento(null);
-
-        //Tranforma o TransacaoDTO em um Transacao
-        Transacao transacao = (Transacao) ModelMapperUtilService.convert(transacaoDTO, Transacao.class);
-
-        transacao.setId(null);
-        transacao.getDescricao().setId(null);
-        transacao.getFormaPagamento().setId(null);
-
-        //When
-
-        //transacao for inválido
-        //A anotação @Valid beerDTO presente no metódo createBeer que realiza tal verificação
-
-        // Então
-
-        //perform: Executa o post /transacao/v1/pagamento
-        //contentType: Define que o tipo do conteúdo é JSON
-        //andExpect: Espera-se que o post retorne o status BadRequest
-
-        mockMvc.perform(post("/transacao/v1/pagamento")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(new Gson().toJson(transacao)))
-                .andExpect(status().isBadRequest());
-
-    }
-
-    // Quando estorno é chamado pelo id, o estorno é retornado
-    @Test
-    void whenReversalCallByIdThenReversalIsReturned() throws Exception {
-
-        // Dado
-
-        //Gera um TransacaoDTO
-        TransacaoDTO transacaoDTO = TransacaoDTOBuilder.builder().build().toTransacaoDTO();
-
-        transacaoDTO.getDescricao().setNsu("1234567890");
-        transacaoDTO.getDescricao().setCodigoAutorizacao("147258369");
-        transacaoDTO.getDescricao().setStatus(StatusEnum.NEGADO);
-
-        //When
-
-        //transacaoService.estornar() -> transacaoDTO
-        when(transacaoService.estornar(1L))
-                .thenReturn(transacaoDTO);
-
-        // Então
-
-        //perform: Executa o post /transacao/v1/pagamento
-        //contentType: Define que o tipo do conteúdo é JSON
-        //content: Define que o conteúdo é o Json de transacaoDTO
-        //andExpect: Espera-se que o post retorne o status Ok
-
-        mockMvc.perform(put("/transacao/v1/estorno/1")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(Math.toIntExact(transacaoDTO.getId()))))
-                .andExpect(jsonPath("$.cartao", is(transacaoDTO.getCartao())))
-                .andExpect(jsonPath("$.descricao.valor", is(transacaoDTO.getDescricao().getValor())))
-                .andExpect(jsonPath("$.descricao.dataHora", is(transacaoDTO.getDescricao().getDataHora())))
-                .andExpect(jsonPath("$.descricao.estabelecimento", is(transacaoDTO.getDescricao().getEstabelecimento())))
-                .andExpect(jsonPath("$.descricao.nsu", is(transacaoDTO.getDescricao().getNsu())))
-                .andExpect(jsonPath("$.descricao.codigoAutorizacao", is(transacaoDTO.getDescricao().getCodigoAutorizacao())))
-                .andExpect(jsonPath("$.descricao.status", is(transacaoDTO.getDescricao().getStatus().toString())))
-                .andExpect(jsonPath("$.formaPagamento.tipo", is(transacaoDTO.getFormaPagamento().getTipo().toString())))
-                .andExpect(jsonPath("$.formaPagamento.parcelas", is(transacaoDTO.getFormaPagamento().getParcelas())));
-
-    }
-
-	 */
 
